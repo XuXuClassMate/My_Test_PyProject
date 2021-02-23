a7e2db5ac02aaf0d7ee3e48529434a6a021532cf
yanxuping <13609394618@163.com>
2021-02-23 23:26:06 +0800

    add wuwork_api test

--- test_API/test_demo.py
+++ test_API/test_demo.py
@@ -1,11 +1,34 @@
 import requests
+import pystache
+import jsonpath
+import yaml
 
 
-class demo:
-    r = requests.get('http://httpbin.testing-studio.com/get')
-    print("json:" + r.json())
-    print("json:" + r.text)
-    print("request:" + r.request)
-    print("cookies:" + r.cookies)
-    print("headers:" + r.headers)
-    assert r.status_code == 200
+class Test_demo:
+    def test_get(self):
+        r = requests.get('http://httpbin.testing-studio.com/get')
+        print("json:" + r.text)
+        print(r.json())
+        print("request:" + str(r.request))
+        print("cookies:" + str(r.cookies))
+        print(r.headers)
+        assert r.status_code == 200
+
+    def test_html(self):
+        html = """
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                 <div class="Popover js-hovercard-content position-absolute" style="display: none; outline: none;" tabindex="0">
+                    <div class="Popover-message Popover-message--bottom-left Popover-message--large Box box-shadow-large" style="width:360px;">
+            </body>
+        </html>"""
+        header = {'Content-Type': 'application/html'}
+        r = requests.post('http://httpbin.org/post', data=html, headers=header)
+        pystache.render({
+            "errcode": 0,
+            "errmsg": "ok",
+            "userid": "{{userid}}"
+        })
+        yaml.safe_load()
+        yaml.load()
---
+++ test_API/test_wuwork_api.py
@@ -0,0 +1,46 @@
+import pytest
+import requests
+
+
+class Test_wuwork_api:
+    def test_get_token(self):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww2ef8a1f13d444dfd&corpsecret=0FzKHtapsva" \
+              "-Uyk8WCURTXE3sXhq6zGsFCg6Yx0BGlU"
+        r = requests.get(url)
+        res = r.json()
+        assert res["errcode"] == 0
+        assert res["access_token"] is not None
+        return res["access_token"]
+
+    data = {
+        "userid": "zhangsan",
+        "name": "张三",
+        "mobile": "+86 13800000002",
+        "department": [1]
+    }
+
+    def test_add_member(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={self.test_get_token()}"
+        r = requests.post(url, json=self.data)
+        res = r.json()
+        assert res['errmsg'] == "created"
+
+    def test_update_muber(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.test_get_token()}"
+        update_data = {
+            "userid": f"{self.data['userid']}",
+            "name": "李四", }
+        r = requests.post(url, json=update_data)
+        res = r.json()
+        assert res['errmsg'] == "updated"
+        print(res)
+
+    def test_delete_muber(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.test_get_token()}&userid={self.data['userid']}"
+        r = requests.get(url)
+        res = r.json()
+        assert res['errmsg'] == "deleted"
+        print(res)
+
+    if __name__ == '__main__':
+        pytest.main(['-vs', 'test_wuwork_api.py'])
