133ab6d67da3af8827170bca53f8d454fdbfad14
yanxuping <13609394618@163.com>
2021-03-07 16:30:23 +0800

    add log print 😋

---
+++ test_API/testing/Logfile.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from datetime import datetime
+
+
+class LogFile(object):
+    def __init__(self):
+        # foldername >> 文件夹名称
+        self.foldername = datetime.now().strftime('%Y-%m-%d')
+        # logfilename >> 文件名称
+        self.logfilename = "logs-" + datetime.now().strftime('%Y-%m-%d-%H') + ".txt"
+
+    def echolog(self, echocontent: str):  # echocontent: str
+        # 获取父级目录
+        path = os.listdir("..")
+        logs = "Logs"
+        if logs not in path:
+            # 创建Logs文件夹
+            os.mkdir("../Logs")
+        path2 = os.listdir("../Logs")
+        if self.foldername not in path2:
+            # 创建文件logfilename >> 文件名称
+            os.mkdir(f"../Logs/{self.foldername}")
+        # 创建日志文件及日志打印
+        with open(f'../Logs/{self.foldername}/{self.logfilename}', 'a') as f:
+            # logtime >> 文件中的时间
+            logtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2]
+            f.write(logtime + " || " + echocontent + "\n")
--- test_API/testing/test_wuwork_api_plus.py
+++ test_API/testing/test_wuwork_api_plus.py
@@ -1,23 +1,32 @@
 from test_API.src.wuwork_api import wuwork_api
 import pytest
+from test_API.testing.Logfile import LogFile
 
 
 class Test_wuwork_api:
     def setup(self):
         self.address = wuwork_api()
+        self.log = LogFile()
 
     def test_add_member(self):
+        self.log.echolog("开始清理数据")
         # 清理数据
         res = self.address.delete_muber(userid="zhangsan")
+        self.log.echolog("清理数据返回json" + str(res))
         pytest.assume(res["errcode"] == 0)
+        self.log.echolog("清理数据完成")
 
         # 查询成员，清理验证
+        self.log.echolog("查询成员，清理验证")
         res = self.address.get_member(userid="zhangsan")
+        self.log.echolog("查询成员返回json" + str(res))
         pytest.assume(res["errcode"] == 60111)
+        self.log.echolog("查询成员完成")
         print(res)
 
         # 创建成员
         res = self.address.add_member(userid="zhangsan", name="张三", mobile="+86 13800000002", department=[1])
+        self.log.echolog("创建成员返回json" + str(res))
         pytest.assume(res["errcode"] == 0)
         # 查询成员,创建验证
         res = self.address.get_member(userid="zhangsan")
@@ -25,10 +34,13 @@ class Test_wuwork_api:
 
         # 更新成员
         res = self.address.update_muber(userid="zhangsan", newname="李四")
+        self.log.echolog("更新成员返回json" + str(res))
         pytest.assume(res["errcode"] == 0)
+        self.log.echolog("更新成员完成")
         # 查询成员，更新验证
         res = self.address.get_member(userid="zhangsan")
         pytest.assume(res["errcode"] == 0)
+        self.log.echolog("查询成员完成")
 
     if __name__ == '__main__':
         pytest.main('vs', 'test_wuwork_api_plus.py')
