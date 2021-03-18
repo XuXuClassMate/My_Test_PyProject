f04fd28238f60c3ed138be7dc50bd3fb5de3b68f
yanxuping <13609394618@163.com>
2021-03-18 14:17:42 +0800

    new add logtime

--- Performance_Test/wuwork_test.py
+++ Performance_Test/wuwork_test.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, task, between
+from datetime import datetime
 
 
 class wuwork_login_and_get_list(HttpUser):
@@ -11,15 +12,15 @@ class wuwork_login_and_get_list(HttpUser):
         r = self.client.get(url, name="token获取").json()
         self.token = r["access_token"]
         if r["errcode"] == 0:
-            print("token获取正确")
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2] + "token获取正确")
         else:
-            print("token获取失败：" + r)
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2] + "token获取失败：" + r)
 
     @task
     def get_member(self, userid="1008611"):
         url = f"cgi-bin/user/get?access_token={self.token}&userid={userid}"
         res = self.client.get(url, name="接口查询").json()
         if res["errcode"] == 0:
-            print("查询验证正确")
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2] + "查询验证正确")
         else:
-            print("查询验证错误：" + res)
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2] + "查询验证错误：" + res)
