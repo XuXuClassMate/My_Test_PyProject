4d2678bd7a8a252fcd3cbf3bd7c1dc9524e70c44
yanxuping <13609394618@163.com>
2021-04-04 21:42:19 +0800

    web ui test for jenkins

---
+++ Jenkins_test/jenkins_Api_test/Readme.md
@@ -0,0 +1,12 @@
+**接口功能自动化测试程序**
+运行环境：
+- python3
+- pytest
+- allure report
+- git
+
+依赖准备：
+pip install allure-pytest
+
+运行命令：
+pytest -sv test/weather_test.py --alluredir ./allure-results
---
+++ Jenkins_test/jenkins_Api_test/test/weather_test.py
@@ -0,0 +1,41 @@
+import allure
+
+from unittest import TestCase
+from library.httpclient import HttpClient
+
+
+@allure.feature('Test Weather api')
+class Weather(TestCase):
+    """Weather api test cases"""
+
+    def setUp(self):
+        """Setup of the test"""
+
+        self.host = 'http://www.weather.com.cn'
+        self.ep_path = '/data/cityinfo'
+        self.client = HttpClient()
+
+    @allure.story('Test of ShenZhen')
+    def test_1(self):
+        city_code = '101280601'
+        exp_city = '深圳'
+        self._test(city_code, exp_city)
+
+    @allure.story('Test of BeiJing')
+    def test_2(self):
+        city_code = '101010100'
+        exp_city = '北京'
+        self._test(city_code, exp_city)
+
+    @allure.story('Test of ShangHai')
+    def test_3(self):
+        city_code = '101020100'
+        exp_city = '上海'
+        self._test(city_code, exp_city)
+
+    def _test(self, city_code, exp_city):
+        url = f'{self.host}{self.ep_path}/{city_code}.html'
+        response = self.client.Get(url=url)
+        act_city = response.json()['weatherinfo']['city']
+        print(f'Expect city = {exp_city}, while actual city = {act_city}')
+        self.assertEqual(exp_city, act_city, f'Expect city = {exp_city}, while actual city = {act_city}')
