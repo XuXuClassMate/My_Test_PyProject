1300b6f529c782a7006aa52a62058061f3e3e3f5
yanxuping <13609394618@163.com>
2021-04-23 10:50:13 +0800

    Update

--- terrace/newprojecttest/src/components/SingIn.vue
+++ terrace/newprojecttest/src/components/SingIn.vue
@@ -1,19 +1,34 @@
 <template>
     <div  class="login-form">
         <h1>登录</h1>
-        <v-text-field label="用户名" type="username"></v-text-field>
-        <v-text-field label="密码" type="password"></v-text-field>
-        <v-btn depressed color="primary">登录</v-btn> 
+        <v-text-field label="用户名" v-model="username" type="username"></v-text-field>
+        <v-text-field label="密码"  v-model="password" type="password"></v-text-field>
+        <v-btn depressed @click="login()" color="primary">登录</v-btn> 
         <v-btn depressed @click="singup()">注册</v-btn>
         <!-- <div style="margin-top: 10px"><v-btn  color="error">找回密码</v-btn></div>   -->
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            username : '',
+            password : ''
+        }
+    },  
     methods:{
         singup(){
             console.log('123')
             this.$router.push({name:'SingUp'})
+        },
+        login(){
+            let post_data = {
+                username: this.username,
+                password: this.password,
+            }
+            this.$api.user.SingIn(post_data).then(res=>{
+                console.log(res)
+            })
         }
     }
     
--- terrace/newprojecttest/src/components/SingUp.vue
+++ terrace/newprojecttest/src/components/SingUp.vue
@@ -3,9 +3,9 @@
         <h1>注册</h1>
         <v-text-field v-model="username" label='用户名' type='user name'></v-text-field>
         <v-text-field v-model="password" label='密码' type='password'></v-text-field>
-        <v-text-field v-model="email" label='邮箱' type='email'></v-text-field>
-        <v-btn color="primary" @click="register()">注册</v-btn>
-        <v-btn color="primary" @click="singIn()" text>去登陆</v-btn>
+        <v-text-field v-model="email" label='邮箱' type_id='email'></v-text-field>
+        <v-btn color="primary" @click="register()" type="register">注册</v-btn>
+        <v-btn color="primary" @click="singIn()" text id="singIn">去登陆</v-btn>
         
     </div>
 
@@ -25,11 +25,10 @@ export default {
             this.$router.push({name:'SingIn'})
         },
         register(){
-            let post_data={
+            let post_data = {
                 username : this.username,
                 password : this.password,
-                email : this.email
-
+                email : this.email,
             }
             this.$api.user.SingUp(post_data).then(res=>{
                 console.log(res)
