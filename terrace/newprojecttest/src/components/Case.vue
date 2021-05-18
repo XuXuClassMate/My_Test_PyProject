c2dbdd61d00d779d29e8b2a0e11d5d8460202a77
bytedancer <13609394618@163.com>
2021-05-18 18:27:24 +0800

    更新日志
    用例页面新增：
    1、新建测试用例
    2、新建测试计划
    3、用例展示

--- terrace/newprojecttest/src/components/Case.vue
+++ terrace/newprojecttest/src/components/Case.vue
@@ -9,7 +9,23 @@
                 <v-tab @click="$router.push({name:'User'})">用户管理</v-tab>
             </v-tabs>
         </template>
-        Case
+        <v-btn color='success' class='btn'>新增计划</v-btn>
+        <v-btn color='primary' class='btn'>新建用例</v-btn>
+        <p>
+            <br>  <!--顶部空行 -->
+            <br/>
+        </p>
+        <template>
+            <v-data-table
+                v-model="selected"
+                :headers="headers"
+                :items="desserts"
+                :single-select="singleSelect"
+                item-key="name"
+                show-select
+                class="elevation-1">
+            </v-data-table>
+        </template>
     </div>
 </template>
 
@@ -17,7 +33,21 @@
 export default {
     data(){
         return{
-
+            selected: [],
+            headers: [{
+                text:'id',
+                value:''
+            },{
+                text:'用例名称',
+                value:''
+            },{
+                text:'数据',
+                value:''
+            },{
+                text:'操作',
+                value:''
+            }], 
+            desserts: []
         }
     },
     methods:{
@@ -27,6 +57,9 @@ export default {
 </script>
 
 <style scoped>
-
+.btn{
+    margin: 10px;
+    float: right   
+}
 </style>
 
