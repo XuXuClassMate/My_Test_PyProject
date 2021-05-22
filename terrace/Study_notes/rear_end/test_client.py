e56bba0c9a13f4d0843b6c17c0bb695244909530
bytedancer <13609394618@163.com>
2021-05-22 10:53:53 +0800

    更新日志
    学习mysql数据库连接基本操作

---
+++ terrace/Study_notes/rear_end/test_client.py
@@ -0,0 +1,24 @@
+import pymysql
+
+    host='stuq.ceshiren.com',
+    user='hogwarts_python',
+    password='hogwarts_python',
+    db='hogwarts_python',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def test_conn():
+    with db.cursor() as cursor:
+        sql = "show tables;"  # 查询数据库中的表名称
+        cursor.execute(sql)
+        print(sql)
+        print(cursor.fetchall())
+
+
+def test_select():
+    with db.cursor() as c:
+        sql = "select * from xsb where username=%s"
+        c.execute(sql, ["demo"])
+        print(c.fetchall())
