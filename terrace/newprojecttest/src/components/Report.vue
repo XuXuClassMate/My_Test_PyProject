0c74c88b9358601f16a0e0ecfa39a5b684f35ead
bytedancer <13609394618@163.com>
2021-05-26 11:12:57 +0800

    更新日志
    新增了新建测试测试任务的功能
    
    add httprunner testfun
    
    Commit log
    
    update use httprunner test fun
    
    更新
    
    更新
    
    update
    
    update
    
    update
    
    1
    
    e56bba0c9a13f4d0843b6c17c0bb695244909530
    
    update
    
    update
    
    Revert "add httprunner testfun"
    
    This reverts commit c38c43983348ec1da805274acb11746ab9c14dfa.

--- terrace/newprojecttest/src/components/Report.vue
+++ terrace/newprojecttest/src/components/Report.vue
@@ -9,6 +9,7 @@
                 <v-tab @click="$router.push({name:'User'})">用户管理</v-tab>
             </v-tabs>
         </template>
+        <div id="main" style="width:500px;height:500px"></div>
         Report
     </div>
 </template>
@@ -22,8 +23,115 @@ export default {
     },
     methods:{
 
+    },
+    mounted(){
+        // // 方法
+        // function initEcharts () {
+        //     // 新建一个promise对象
+        //     let newPromise = new Promise((resolve) => {
+        //         resolve()
+        //     })
+        //     //然后异步执行echarts的初始化函数
+        //     newPromise.then(() => {
+        //         //	此dom为echarts图标展示dom
+        //         echarts.init(DOm)
+        //     })
+        // };
+
+        var echarts = require('echarts');
+        // 基于准备好的dom，初始化echarts实例
+        // var myChart = echarts.init(document.getElementById('mian'))
+        // var option;
+        var chartDom = document.getElementById('main');
+        console.log(chartDom)
+        var myChart = echarts.init(chartDom);
+        var option;
+        //绘制图表
+        option = {
+            title: {
+                text: '未来一周气温变化',
+                subtext: '纯属虚构'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['最高气温', '最低气温']
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    dataView: {readOnly: false},
+                    magicType: {type: ['line', 'bar']},
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value} °C'
+                }
+            },
+            series: [
+                {
+                    name: '最高气温',
+                    type: 'line',
+                    data: [10, 11, 13, 11, 12, 12, 9],
+                    markPoint: {
+                        data: [
+                            {type: 'max', name: '最大值'},
+                            {type: 'min', name: '最小值'}
+                        ]
+                    },
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'}
+                        ]
+                    }
+                },
+                {
+                    name: '最低气温',
+                    type: 'line',
+                    data: [1, -2, 2, 5, 3, 2, 0],
+                    markPoint: {
+                        data: [
+                            {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
+                        ]
+                    },
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'},
+                            [{
+                                symbol: 'none',
+                                x: '90%',
+                                yAxis: 'max'
+                            }, {
+                                symbol: 'circle',
+                                label: {
+                                    position: 'start',
+                                    formatter: '最大值'
+                                },
+                                type: 'max',
+                                name: '最高点'
+                            }]
+                        ]
+                    }
+                }
+            ]
+        };
+        option && myChart.setOption(option);
+//下面这里不是
+        }
     }
-}
 </script>
 
 <style scoped>
