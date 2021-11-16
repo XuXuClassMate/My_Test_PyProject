942be087cbc55cfc027f096c44bae7e81002514d
旭旭同学 <13609394618@163.com>
2021-11-16 17:42:05 +0800

    add thread and multi

--- test_API/testing/Logfile.py
+++ test_API/testing/Logfile.py
@@ -1,14 +1,13 @@
 import os
-import shutil
 from datetime import datetime
 
 
 class LogFile(object):
     def __init__(self):
         # foldername >> 文件夹名称
-        self.foldername = datetime.now().strftime('%Y-%m-%d')
+        self._foldername = datetime.now().strftime('%Y-%m-%d')
         # logfilename >> 文件名称
-        self.logfilename = "logs-" + datetime.now().strftime('%Y-%m-%d-%H') + ".txt"
+        self._logfilename = "logs-" + datetime.now().strftime('%Y-%m-%d-%H') + ".txt"
 
     def echolog(self, echocontent: str):  # echocontent: str
         # 获取父级目录
@@ -18,11 +17,11 @@ class LogFile(object):
             # 创建Logs文件夹
             os.mkdir("../Logs")
         path2 = os.listdir("../Logs")
-        if self.foldername not in path2:
+        if self._foldername not in path2:
             # 创建文件logfilename >> 文件名称
-            os.mkdir(f"../Logs/{self.foldername}")
+            os.mkdir(f"../Logs/{self._foldername}")
         # 创建日志文件及日志打印
-        with open(f'../Logs/{self.foldername}/{self.logfilename}', 'a') as f:
+        with open(f'../Logs/{self._foldername}/{self._logfilename}', 'a') as f:
             # logtime >> 文件中的时间
             logtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S  %f')[:-2]
             f.write(logtime + " || " + echocontent + "\n")
