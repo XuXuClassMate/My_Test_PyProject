54a43a0e7519264d22bd572342e9056300f7c553
bytedancer-YXP <13609394618@163.com>
2021-11-16 09:32:17 +0800

    update

--- terrace/backend/src/testcase/TestCase_Text.py
+++ terrace/backend/src/testcase/TestCase_Text.py
@@ -20,21 +20,21 @@ app.config['AQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lagou5:hogwarts@stuq.ce
 db = SQLAlchemy(app)
 jenkins = Jenkins(
     'http://localhost:8080/',
-    username = "admin",
+    username="admin",
     # 需要用户管理添加token
-    password = "11ec796312b80c8841f4eef5394b74af26"
-    )
+    password="11ec796312b80c8841f4eef5394b74af26"
+)
 
 
 # 测试用例的管理
 class TestCase_Text(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     # 测试名称 80个字符，不是唯一，不能为空
-    name = db.Column(db.String(80), unique = False, nullable = False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     # 用例为文本数据类型
-    data = db.Column(db.String(1000), unique = False, nullable = False)
+    data = db.Column(db.String(1000), unique=False, nullable=False)
     # 备注 ，1000个字符，允许不唯一，允许为空
-    remark = db.Column(db.String(1000), unique = False, nullable = True)
+    remark = db.Column(db.String(1000), unique=False, nullable=True)
 
     def __repr__(self):
         return '<TestCase_Text %r>' % self.name
@@ -55,11 +55,11 @@ class TestCaseTextService(Resource):
 
     def post(self):
         testcase = TestCase_Text(
-            id = request.json.get('id'),
-            name = request.json.get('name'),
-            data = request.json.get('data'),
-            remark = json.dumps(request.json.get('remark'))
-            )
+            id=request.json.get('id'),
+            name=request.json.get('name'),
+            data=request.json.get('data'),
+            remark=json.dumps(request.json.get('remark'))
+        )
         db.session.add(testcase)
         db.session.commit()
 
@@ -68,11 +68,11 @@ class TestCaseTextService(Resource):
 
     def put(self):
         testcase = TestCase_Text(
-            id = request.json.get('id'),
-            name = request.json.get('name'),
-            data = request.json.get('data'),
-            remark = json.dumps(request.json.get('remark'))
-            )
+            id=request.json.get('id'),
+            name=request.json.get('name'),
+            data=request.json.get('data'),
+            remark=json.dumps(request.json.get('remark'))
+        )
         db.session.add(testcase)
         db.session.commit()
 
@@ -84,7 +84,7 @@ class TestCaseTextService(Resource):
         # for item in testcase_id:
         #     if item['id'] == testcase_id:
         #         item.remove(testcase_id)
-        testcase = TestCase_Text.query.filter_by(id = testcase_id).first()
+        testcase = TestCase_Text.query.filter_by(id=testcase_id).first()
         testcases = TestCase_Text.query.all()
         db.session.delete(testcase)
         return [str(testcase) for testcase in testcases]
@@ -101,16 +101,16 @@ suite [test_1.py::test_a, test_1.py::test_b,test_2.py::test_c]
 
 
 class Suite(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), unique = False, nullable = False)
-    testcases = db.Column(db.String(1000), unique = False, nullable = True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    testcases = db.Column(db.String(1000), unique=False, nullable=True)
 
     def as_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'testcases': json.loads(self.testcases)
-            }
+        }
 
     def __repr__(self):
         return f'<Suite id={self.id}    {self.name}>'
@@ -121,7 +121,7 @@ class SuiteService:
         id = request.args.get('id', None)
         app.logger.info({'id': id})
         if id:
-            suite = Suite.query.filter_by(id = id).first()
+            suite = Suite.query.filter_by(id=id).first()
             return str(suite)
         else:
             suites = Suite.query.all()
@@ -129,9 +129,9 @@ class SuiteService:
 
     def post(self):
         suite = Suite(
-            name = request.json.get('name'),
-            testcases = json.dumps(request.json.get('testcases'))
-            )
+            name=request.json.get('name'),
+            testcases=json.dumps(request.json.get('testcases'))
+        )
         db.session.add(suite)
         db.session.commit()
 
@@ -143,14 +143,14 @@ class SuiteService:
 class ExcutionService(Resource):
     def post(self):
         suite_id = request.json.get('suite_id')
-        suite = Suite.query.filter_by(id = suite_id)
+        suite = Suite.query.filter_by(id=suite_id)
 
         jenkins.jobs['testcase_id'].invoke(
-            build_params = {
+            build_params={
                 'suite': json.dumps(suite.as_dict()),
                 'command': f'git clone https://github.com/yanxuping/My_Test_Project.git .; pytest {suite.testcases}'
-                }
-            )
+            }
+        )
 
 
 class Result:
@@ -167,4 +167,4 @@ api.add_resource(SuiteService, '/suite')
 api.add_resource(ExcutionService, '/execution')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
