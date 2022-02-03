68724c7d2a40922547975d1e0d6183c111a7444d
旭旭同学 <13609394618@163.com>
2022-02-04 07:44:54 +0800

    Update myweb

---
+++ test/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*- 
+# @Author : XuXu ClassMate
+# @File : __init__.py.py 
+# username： xuxudemac
+# @IDE: PyCharm
+# @Time : 2022/1/27 10:04 上午 
---
+++ test/testplus.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*- 
+# @Author : XuXu ClassMate
+# @File : testplus.py 
+# username： xuxudemac
+# @IDE: PyCharm
+# @Time : 2022/1/27 10:04 上午 
